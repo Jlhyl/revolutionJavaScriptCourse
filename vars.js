b49d26e8057e0633d391918311c5493c35662807
riseRevolution <rodharoldrod@gmail.com>
2022-04-16 20:43:10 -0500

    Primer Commit - Se realiza como inicio del curso de JavaScript con CoderHouse

---
+++ vars.js
@@ -0,0 +1,6 @@
+// Visualización del log apartir desde el documento.
+    console.log("Importación del Log desde el documento JS")
+
+// Comentario en JavaScript
+    alert("La primera Alerta /giiiiyuuu/")
+    
