d5209328774bb9659de7467ba2b4cdca4a2ee73e
riseRevolution <rodharoldrod@gmail.com>
2022-04-18 08:26:41 -0500

    Commit de Intervención uno en el repositorio - Clase 1

--- vars.js
+++ vars.js
@@ -3,4 +3,67 @@
 
 // Comentario en JavaScript
     alert("La primera Alerta /giiiiyuuu/")
+
+    // Declaración de Variable con var
+    var name = 'Rise Revolution Team'
+
+    // Declaración de Variable con - ES6
+    let age = 30
+    const firstLastname = 'Rodriguez'
+
+    // Declaración de Variable sin valor    
+    let global
+
+    //
+
+    let usado = 17
+    usado = 25
+
+    console.log(usado)
+
+    // Operaciones Básicas
+
+    let numA = 5, numbB=8
+    const numC = 10
     
+    let resultSuma = numA + numbB
+    console.log('La Suma de los dos primeros números es :', resultSuma)
+
+    let resultResta = numA - numbB
+    console.log('La Resta de los dos primeros números es :', resultResta)
+
+    let resultMult = numA * numbB
+    console.log('La Multiplicación de los dos primeros números es :', resultMult)
+    
+    let resultDiv = numC / numA
+    console.log('La División entre la constante y el número A es :', resultDiv)
+
+    alert ('Result Div es : ' + resultDiv)
+
+    // Concatenación de Caracteres, al incluirse un número se trata como string
+
+    let textA = 'Hola', textB = 'Qué hace?', textC = 'Aún Atrasado?', espacio = ' '
+
+    let resulText = textA + espacio + textB + espacio + textC
+    console.log('La cadena de texto dice:', resulText)
+
+    let resulText2 = textA + espacio + textB + espacio + textC + espacio + 25
+    console.log('La cadena de texto dice:', resulText2)
+
+    // Uso de Prompt, para solicitar información al usuario
+
+    let marcaSol = prompt('La marca del Carro deseado es')
+    console.log('El usuario escogio un carro de marca :', marcaSol) 
+
+    // Suma de dos datos numericos por entrada de usuario
+
+    let num1 = prompt('Solo ingrese el primer dato a sumar'), num2 = prompt('Solo ingrese el segundo dato a sumar')
+    num1 = parseInt(num1), num2 = parseInt(num2)
+
+    const sumaNumIn = num1 + num2
+    console.log('La suma de los valores ingresados es :', sumaNumIn) 
+
+    var unNum = 30, unNumb='20'
+        
+    let resultS = unNum + unNumb
+    console.log('La Suma de los dos primeros números es :', resultS)
